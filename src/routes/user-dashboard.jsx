309b7bae699b08275843d51539a829f32a7d518f
devtescode <teslimagboola09@gmail.com>
2026-06-05 10:43:06 +0100

    update code and fixed bug

--- src/routes/user-dashboard.jsx
+++ src/routes/user-dashboard.jsx
@@ -156,7 +156,7 @@ function UserDashboard() {
           >
             {rushHour === true
               ? "Rush Hour is active. You can place orders online."
-              : "crowds are currently low. Please visit Dunnkayce physically."}
+              : "crowds are currently low. You can visit Dunnkayce physically."}
           </div>
         </div>
       </section>
